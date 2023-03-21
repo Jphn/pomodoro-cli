40eeba2e19720d15f175b78e9618b6552ab93a8a
Jphn <joaopedroholandaneves@gmail.com>
2023-03-21 14:08:43 -0300

    feat(colorize): create module

---
+++ src/modules/colorize.h
@@ -0,0 +1,14 @@
+#ifndef COLORIZE_H
+#define COLORIZE_H
+
+#define BOLD "\e[1m"
+#define REGULAR "\e[0m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define PURPLE "\033[35m"
+#define CYAN "\033[36m"
+#define WHITE "\033[37m"
+
+#endif
