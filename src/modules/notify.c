6339a795cb2fd911bbf37410dcaadc5514f308d1
Jphn <joaopedroholandaneves@gmail.com>
2023-03-24 14:14:20 -0300

    feature(notify): create first look

---
+++ src/modules/notify.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "notify.h"
+
+void notify(char *title, char *message, int urgencyLevel)
+{
+	char *urgency, command[300];
+
+	switch (urgencyLevel)
+	{
+	case 1:
+		urgency = "low";
+		break;
+
+	default:
+		urgency = "normal";
+		break;
+
+	case 3:
+		urgency = "critical";
+		break;
+	}
+
+	sprintf(command, "notify-send -u %s \"%s\" \"%s\"", urgency, title, message);
+
+	system(command);
+}
---
+++ src/modules/notify.h
@@ -0,0 +1,6 @@
+#ifndef NOTIFY_H
+#define NOTIFY_H
+
+void notify(char title[100], char message[150], int urgency);
+
+#endif
