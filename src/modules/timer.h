a4ae07a4cbd7951ab75499cb783ae41675ac1462
Jphn <joaopedroholandaneves@gmail.com>
2023-03-26 17:56:02 -0300

    doc(timer): add all functions descriptions

--- src/modules/timer.h
+++ src/modules/timer.h
@@ -1,7 +1,19 @@
 #ifndef TIMER_H
 #define TIMER_H
 
+/**
+ * @brief Atrasa a execução do programa em segundos conforme o valor determinado.
+ *
+ * @param seconds O número inteiro de segundos.
+ */
 extern void delay_s(int seconds);
+
+/**
+ * @brief Atrasa a execução do programa em minutos conforme o valor determinado.
+ *
+ * @param minutes O número inteiro de minutos.
+ * @param onSecPass Função a ser executada conforme a passagem dos segundos.
+ */
 extern void delay_m(int minutes, void (*onSecPass)(int, int, float));
 
 #endif
