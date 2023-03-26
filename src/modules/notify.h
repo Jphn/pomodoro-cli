f1d81fb398a75d68cd94f71619d743f16146db22
Jphn <joaopedroholandaneves@gmail.com>
2023-03-26 17:54:52 -0300

    doc(notify): add notify  function description

--- src/modules/notify.h
+++ src/modules/notify.h
@@ -1,6 +1,13 @@
 #ifndef NOTIFY_H
 #define NOTIFY_H
 
+/**
+ * @brief Emite uma notificação que será exibida na tela do usuário.
+ *
+ * @param title Título que será exibido na notificação.
+ * @param message Mensagem contida no corpo da notificação.
+ * @param urgency Nível de importância da notificação, pode variar de 1 à 3.
+ */
 extern void notify(char *title, char *message, int urgency);
 
 #endif
