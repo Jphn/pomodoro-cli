c39379199b17670f201455cb0611d2c58a026581
Jphn <joaopedroholandaneves@gmail.com>
2023-03-25 22:46:03 -0300

    feature(handleArgs): add option to change settings via cli

--- src/main.c
+++ src/main.c
@@ -3,9 +3,36 @@
 #include <string.h>
 #include "./modules/pmanager.h"
 
-int main()
+void handleArgs(int argc, char **argv);
+
+int main(int argc, char **argv)
 {
+	handleArgs(argc, argv);
+
 	manager();
 
 	return 0;
+}
+
+void handleArgs(int argc, char **argv)
+{
+	for (int c = 1; c < argc; c++)
+		switch (argv[c][1])
+		{
+		case 'f':
+			cycle[0] = atoi(argv[++c]);
+			break;
+
+		case 's':
+			cycle[1] = atoi(argv[++c]);
+			break;
+
+		case 'l':
+			cycle[2] = atoi(argv[++c]);
+			break;
+
+		case 'i':
+			cycle[3] = atoi(argv[++c]);
+			break;
+		}
 }
